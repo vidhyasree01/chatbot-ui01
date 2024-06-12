1ebd5e0286f275edb945557b86f96b38c27e945f
vidhyasree01 <vidhya@decencode.com>
2024-06-12 19:40:03 +0530

    update the changes

--- next.config.js
+++ next.config.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true"
-})
-
-const withPWA = require("next-pwa")({
-  dest: "public"
-})
-
-module.exports = withBundleAnalyzer(
-  withPWA({
-    reactStrictMode: true,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "localhost"
-        },
-        {
-          protocol: "http",
-          hostname: "127.0.0.1"
-        },
-        {
-          protocol: "https",
-          hostname: "**"
-        }
-      ]
-    },
-    experimental: {
-      serverComponentsExternalPackages: ["sharp", "onnxruntime-node"]
-    }
-  })
-)
-=======
 const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
@@ -50,4 +16,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
->>>>>>> source/legacy
